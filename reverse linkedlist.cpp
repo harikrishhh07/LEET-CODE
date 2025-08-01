e2a05da8e153d195c6bcb7b3bc46bb0099a6b60c
Harikrishna <ee3962@srmist.edu.in>
2025-08-01 23:55:14 +0530

    Add files via upload

---
+++ reverse linkedlist.cpp
@@ -0,0 +1,10 @@
+ListNode* reverseList(ListNode* head) {
+ListNode* prev = nullptr;
+while (head) {
+ListNode* nextNode = head->next;
+head->next = prev;
+prev = head;
+head = nextNode;
+}
+return prev;
+}
