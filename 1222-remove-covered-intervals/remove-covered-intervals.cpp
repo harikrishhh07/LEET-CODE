194c12e2129f1abd921bebab188a63612313cad1
E.HARIKRISHNA <harielamvazuthi@gmail.com>
2026-07-06 08:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (30.60%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+
+        int count = 0;
+        int end = 0;
+
+        for (auto &interval : intervals) {
+            if (interval[1] > end) {
+                count++;
+                end = interval[1];
+            }
+        }
+
+        return count;
+    }
+};
